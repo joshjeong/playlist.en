2b09c922a361c0380768652e3e6e3dac18314f5e
Josh Jeong <jcjeong@gmail.com>
2014-08-11 16:51:17 -0700

    Added host and track_num attributes to room

--- db/migrate/20140811005835_create_rooms.rb
+++ db/migrate/20140811005835_create_rooms.rb
@@ -2,7 +2,8 @@ class CreateRooms < ActiveRecord::Migration
   def change
     create_table :rooms do |t|
       t.string :name, :playlist
-
+      t.boolean :host
+      t.integer :track_num
       t.timestamps
     end
   end
