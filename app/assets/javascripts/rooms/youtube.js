ea799338131734a0d072890717bc4077e7cbc166
Josh Jeong <jcjeong@gmail.com>
2014-10-05 22:37:33 -0700

    Creates youtube js player

--- app/assets/javascripts/rooms/youtube.js
+++ app/assets/javascripts/rooms/youtube.js
@@ -1,11 +1,11 @@
 
-var Youtube_player = {}
+var Youtube.Controller = {}
 
-Youtube_player = function(){
+Youtube.Controller = function(){
 
  var params = { allowScriptAccess: "always" };
     var atts = { id: "myytplayer" };
-    swfobject.embedSWF("http://www.youtube.com/v/VIDEO_ID?enablejsapi=1&playerapiid=ytplayer&version=3",
+    swfobject.embedSWF("http://www.youtube.com/v/lK4red5MqBI?enablejsapi=1&playerapiid=ytplayer&version=3",
                        "ytapiplayer", "425", "356", "8", null, null, params, atts);
   
 }
