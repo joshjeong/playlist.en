0016b4c57ae6c4c779568018a992f06db872b680
Josh Jeong <jcjeong@gmail.com>
2014-10-06 12:22:38 -0700

    Refactors ytplayer options

--- app/assets/javascripts/rooms/youtube.js
+++ app/assets/javascripts/rooms/youtube.js
@@ -7,11 +7,15 @@ Youtube.Controller = function(){
 
   var params = { allowScriptAccess: "always" };
       atts = { id: "myytplayer" };
+      settings = "?enablejsapi=1&playerapiid=ytplayer&version=3"
       video_id = "lK4red5MqBI"
-
+      and = "&"
+      autoplay = "autoplay=1"
+      controls = "controls=0"
+      info = "showinfo=0"
   this.createPlayer = function(){
 
-  swfobject.embedSWF("http://www.youtube.com/v/"+ video_id + "?enablejsapi=1&playerapiid=ytplayer&version=3&autoplay=1&controls=0&rel=0&showinfo=0&autohide=1&color=white&iv_load_policy=3&theme=light",
+  swfobject.embedSWF("http://www.youtube.com/v/"+video_id+settings+and+autoplay+and+controls+and+info,
                     "ytapiplayer", "425", "356", "8", null, null, params, atts);
   }
 }
