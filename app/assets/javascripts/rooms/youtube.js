95980e964aa22c432a44f017c85207a6562641d0
Josh Jeong <jcjeong@gmail.com>
2014-10-07 14:33:17 -0700

    AJAXifies first video search

--- app/assets/javascripts/rooms/youtube.js
+++ app/assets/javascripts/rooms/youtube.js
@@ -8,6 +8,7 @@ Youtube.Controller = function(){
 
   this.bindListeners = function(){
     this.selectSongListener();
+    this.searchSongListener();
   }
 
   this.createPlayer = function(){
@@ -23,6 +24,28 @@ Youtube.Controller = function(){
     swfobject.embedSWF("http://www.youtube.com/v/"+video_id+settings+and+autoplay+and+controls+and+info,
                     "ytapiplayer", "425", "356", "8", null, null, params, atts);
   }
+
+  this.searchSongListener = function(){
+    $('#search_first_song').on('submit', function(e){
+      e.preventDefault();
+      self.searchSong($(this));
+    })
+    
+  }
+
+  this.searchSong = function(form){
+    var url = form.attr("action")
+        searchQuery = form.find('input').first().val()
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: {search: searchQuery}
+    }).done(function(response){
+      $('#search_first_song').append(response)
+      $('#search_first_song').find('input').first().val("")
+    })
+  }
+
 
   this.selectSongListener = function(){
     $('.video-container').on('click',function(e){
--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -1,5 +1,6 @@
 require 'pp'
 class TracksController < ApplicationController
+  skip_before_filter :verify_authenticity_token, :only => :create
 
   def index
     @room = Room.find_by('name=?', params[:room_id])
@@ -8,7 +9,7 @@ class TracksController < ApplicationController
   end
 
   def create
-    search_query = params[:track][:video_id]
+    search_query = params[:search]
     @room = Room.find(params[:room_id])
     client ||= YouTubeIt::Client.new(:dev_key => ENV["API_KEY"])
     @results = client.videos_by(:query => search_query, :page => 1, :per_page => 10)
