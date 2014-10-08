d7bc93939d71d7b7c2b686f504678a9f0c17726c
Josh Jeong <jcjeong@gmail.com>
2014-10-07 17:43:55 -0700

    Adds logic to room and track controller

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -8,11 +8,22 @@ class TracksController < ApplicationController
   end
 
   def create
-    search_query = params[:search]
-    @room = Room.find(params[:room_id])
     client ||= YouTubeIt::Client.new(:dev_key => ENV["API_KEY"])
-    @results = client.videos_by(:query => search_query, :page => 1, :per_page => 10)
-    render :show
+    @room = Room.find(params[:room_id])
+    if params[:search]
+      search_query = params[:search]
+      @room.tracks.create(video_id: search_query)
+      @results = client.videos_by(:query => search_query, :page => 1, :per_page => 10)
+      render :show
+    elsif params[:nextSearch]
+      search_query = params[:nextSearch]
+      @room.tracks.create(video_id: search_query)
+      @results = client.videos_by(:query => search_query, :page => 1, :per_page => 10)
+      render :next_song_results
+    else
+      @room.tracks.create(video_id: params[:addVideo])
+      render :song_added
+    end
   end
 
   def show
