2ebf99f8e020d7160139bc9fcca9efe673e43b9d
Josh Jeong <jcjeong@gmail.com>
2014-10-12 21:42:35 -0700

    Reorganize routing logic to be more efficient

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -5,14 +5,31 @@ class RoomsController < ApplicationController
 
   def create
     room_name = params[:room][:name]
-    Room.create(name: room_name) if Room.find_by(name: room_name)==nil
-    redirect_to room_tracks_path(room_id: room_name)
+    if Room.find_by(name: room_name)==nil
+      Room.create(name: room_name)
+      redirect_to search_rooms_path
+    else
+      redirect_to 
   end
 
   def show
-    @room_name = params[:id]
-    @room = Room.find_by('name=?', "#{@room_name}")
-    @video_id = params[:first]
+    @room = Room.find_by('name=?', "#{params[:id]}")
+    @video = params[:addVideo] 
+    render :show
+  end
+
+  def search
+    client ||= YouTubeIt::Client.new(:dev_key => ENV["API_KEY"])
+    @room = Room.find(params[:room_id])
+    if params[:search]
+      search_query = params[:search]
+      @room.tracks.create(video_id: search_query)
+      @results = client.videos_by(:query => search_query, :page => 1, :per_page => 10)
+      render :video_result
+    else
+      @room.tracks.create(video_id: params[:addVideo])
+      render :song_added
+    end
   end
 
   
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   resources :rooms do
+    collection do
+      post 'search'
+    end
     resources :tracks
   end
   root 'rooms#index'
