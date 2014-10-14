6a026b81fa1b02e36b57ed1c4ed5802397b825dd
Josh Jeong <jcjeong@gmail.com>
2014-10-13 19:43:11 -0700

    Creates route and view for guest

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -7,12 +7,14 @@ class RoomsController < ApplicationController
   def create
     room_name = params[:room][:name]
     session[:queue] = []
-    # if Room.find_by(name: room_name)==nil
+    if Room.find_by(name: room_name)==nil
       Room.create(name: room_name)
       @room = Room.find_by(name: room_name)
       redirect_to room_path(id: @room.name)
-    # else
-    #   redirect_to 
+    else
+      binding.pry
+      redirect_to guest_room_path(id: room_name)
+    end
   end
 
   def show
@@ -26,7 +28,6 @@ class RoomsController < ApplicationController
     @room = Room.find_by('name=?', params[:id])
     if params[:search]
       search_query = params[:search]
-      @room.tracks.create(video_id: search_query)
       @results = @client.videos_by(:query => search_query, :page => 1, :per_page => 10)
       render :video_result
     else
@@ -38,19 +39,33 @@ class RoomsController < ApplicationController
     @room = Room.find_by('name=?', params[:id])
     @room.tracks.create(video_id: params[:video_id])
     @new_video = @room.tracks.find_by('video_id=?', params[:video_id])
-    session[:queue] << @new_video.video_id
+    # session[:queue] << @new_video.video_id
     if params[:not_first_song]
+      binding.pry
       render :song_added
     else
-      @next_video = session[:queue].shift
+      next_video_obj = @room.tracks.first
+      @room.tracks.delete(next_video_obj)
+      @next_video = next_video_obj.video_id
       render :theatre
     end
   end
 
   def nextvideo
-    @next_video = session[:queue].shift
+    @room = Room.find_by('name=?', params[:id])
+    next_video_obj = @room.tracks.first
+    @room.tracks.delete(next_video_obj)
+    @next_video = next_video_obj.video_id
     render json: {video: @next_video}
   end
 
+  def guest
+  end
+
+  def guestsearch
+    @room = Room.find_by('name=?', params[:id])
+    @room.tracks.create(video_id: params[:video_id])
+    render :song_added
+  end
 
 end
