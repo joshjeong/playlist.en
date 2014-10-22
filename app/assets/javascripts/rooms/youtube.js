832a31abeaf87ecf8cccf47e8f05cf749805045c
Josh Jeong <jcjeong@gmail.com>
2014-10-22 16:06:11 -0700

    Refactors js

--- app/assets/javascripts/rooms/youtube.js
+++ app/assets/javascripts/rooms/youtube.js
@@ -1,70 +1,73 @@
-Youtube.Controller = function(){
-    var tag = document.createElement('script');
-        tag.src = "https://www.youtube.com/iframe_api";
-        firstScriptTag = document.getElementsByTagName('script')[0];
-        firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-        done = false;
-        window.player;
+// init=function(){
+//   var tag = document.createElement('script');
+//       tag.src = "https://www.youtube.com/iframe_api";
+//       firstScriptTag = document.getElementsByTagName('script')[0];
+//       firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+//       done = false;
+//       window.player;
+// }
 
 
-    onYouTubeIframeAPIReady =function() {
-      window.player = new YT.Player('player', {
-        height: '100%',
-        width: '100%',
-        videoId: "#{@next_video}",
-        playerVars:
-        {
-          controls: 1,
-          showinfo: 0,
-          autoplay: 1
-        },
-        events: {
-          'onReady': onReady,
-          'onStateChange': onStateChange
-        }
-      });    
-    }
+// onYouTubeIframeAPIReady =function() {
+  
+//   window.player = new YT.Player('player', {
+//     height: '100%',
+//     width: '100%',
+//     videoId: "#{@next_video}",
+//     playerVars:
+//     {
+//       controls: 1,
+//       showinfo: 0,
+//       autoplay: 1
+//     },
+//     events: {
+//       'onReady': onReady,
+//       'onStateChange': onStateChange
+//     }
+//   });    
+// }
 
-    onReady =function() {
-        window.player.addEventListener('onStateChange', function(e) {
-          var firstVideo = true;
-          if ( e.data == 0) {
-            console.log('song has ended')
-            window.player.destroy();
-            $.ajax({
-              url: window.location.pathname+ '/nextvideo',
-              type: 'post'
-            }).done(function(response){
-              playNextVideo(response.video);
-            })
-          }
-        });
-      }
+// onReady =function() {
+//   window.player.addEventListener('onStateChange', function(e) {
+//     var firstVideo = true;
+//     if ( e.data == 0) {
+//       console.log('song has ended')
+//       window.player.destroy();
+//       $.ajax({
+//         url: window.location.pathname+ '/nextvideo',
+//         type: 'post'
+//       }).done(function(response){
+//         playNextVideo(response.video);
+//       })
+//     }
+//   });
+// }
 
-        onStateChange =function(newState) {
-          if ( newState == 0 ) {
-        }
-      }
+// onStateChange =function(newState) {
+//     if ( newState == 0 ) {
+//   }
+// }
 
-      playNextVideo =function(video) {
-        console.log('play next video')
-        console.log(video)
 
-        window.player = new YT.Player('player', {
-          height: '100%',
-          width: '100%',
-          videoId: video,
-          playerVars:
-            {
-              controls: 1,
-              showinfo:0,
-              autoplay: 1
-            },
-          events: {
-            'onReady': onReady,
-            'onStateChange': onStateChange
-          }
-        });    
-      }
+// Youtube.Controller = function(){
 
-  }
+//       this.playNextVideo =function(video) {
+
+//         window.player = new YT.Player('player', {
+//           height: '100%',
+//           width: '100%',
+//           videoId: video,
+//           playerVars:
+//             {
+//               controls: 1,
+//               showinfo:0,
+//               autoplay: 1
+//             },
+//           events: {
+//             'onReady': onReady,
+//             'onStateChange': onStateChange
+//           }
+//         });    
+//       }
+
+//   }
