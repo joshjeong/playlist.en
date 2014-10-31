730587895941910afe601945e8897d4bd296135e
Josh Jeong <jcjeong@gmail.com>
2014-10-31 12:25:38 -0700

    Separates view from controller

--- app/assets/javascripts/rooms/theatre.js
+++ app/assets/javascripts/rooms/theatre.js
@@ -1,17 +1,20 @@
-Theatre.Controller = function(searchView) {
-  this.sView = searchView
+Theatre.Controller = function(searchView, theatreView) {
+  this.sView = new searchView;
+  this.tView = new theatreView;
 }
 
 Theatre.Controller.prototype = {
   bindListeners: function(){
     this.playlistBtnListener();
     this.searchBtnListener();
+    this.showCloseBtnListener();
+    this.closeListListener();
   },
 
   playlistBtnListener: function(){
     var self = this;
     $('#playlist-btn').on('click', function(e){
-      self.playlistBtn($(this));
+      self.tView.showPlaylist();
     })
   },
 
@@ -22,17 +25,52 @@ Theatre.Controller.prototype = {
     })
   },  
 
-  playlistBtn: function(button){
-    var url = window.location.pathname + '/playlist'
+  showCloseBtnListener: function(){
+    var self = this;
+    $('#playlist-btn').on('click', function(){
+      self.tView.showCloseBtn();
+    })
+  },
+
+  closeListListener: function(){
+    var self = this;
+    $('#close-list-btn').on('click', function(){
+      self.tView.hidePlaylist();
+    })
+  },
+
+  getPlaylist: function(){
+    var url = window.location.pathname
+        self = this
     $.ajax({
-      url: url,
+      url: url + "/get_playlist",
       type: "GET"
     }).done(function(response){
+      $('#playlist-container').append(response)
     })
   },
 
   searchBtn: function(button){
     this.sView.showSearch();
   }
+}
+
+Theatre.View = function(){}
+
+Theatre.View.prototype = {
+  showCloseBtn: function(){
+    $('#close-list-btn').css('visibility', 'visible')
+  },
+  
+  showPlaylist: function(){
+    $('#playlist-container').css('visibility', 'visible')
+  },
+
+  hidePlaylist: function(){
+    $('#close-list-btn').css('visibility', 'hidden')
+    $('#playlist-container').css('visibility', 'hidden')
+  }
+
+
 }
 
